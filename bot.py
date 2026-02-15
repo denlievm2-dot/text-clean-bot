ddc71418b77747397f02e2ff4605ddc6b0bfc76e
denlievm2-dot <denlievm2@gmail.com>
2026-02-15 20:24:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,29 +6,28 @@ import emoji
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 from aiogram.filters import CommandStart
-from aiogram.enums import ParseMode
 
 TOKEN = os.getenv("TOKEN")
 
 PREFIX_TEXT = "аккаунт зарегистрирован на 15 летнюю девочку,которая опять же пойдет в полицию со мной,ну можем мирно решить,выбор только за тобой,двоих уже так закрыли,также переписка будет отправлена родственникам и знакомым. Выбор за тобой как разойтись,мирно или по плохому.\n\n"
 
-bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
+bot = Bot(token=TOKEN)
 dp = Dispatcher()
+
 
 def remove_emoji(text):
     return emoji.replace_emoji(text, replace='')
+
 
 def remove_interest_line(text):
     pattern = r"👁?\s*Интересовались этим:.*"
     return re.sub(pattern, "", text)
 
-def make_links_clickable(text):
-    url_pattern = r'(https?://\S+)'
-    return re.sub(url_pattern, r'<a href="\1">\1</a>', text)
 
 @dp.message(CommandStart())
 async def start(message: Message):
     await message.answer("Отправь текст для обработки.")
+
 
 @dp.message()
 async def clean(message: Message):
@@ -36,12 +35,13 @@ async def clean(message: Message):
         text = message.text
         text = remove_emoji(text)
         text = remove_interest_line(text)
-        text = make_links_clickable(text)
         text = text.strip()
         await message.answer(PREFIX_TEXT + text)
 
+
 async def main():
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
